b3dd3e7fb03b422031a87bea804d9a1a98c90654
Harsh verma <chacha45454@gmail.com>
2025-05-14 16:43:50 +0530

    api live

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const local_url = 'mongodb://127.0.0.1:27017/api'
+const live_URL = 'mongodb+srv://chacha45454:ram123@cluster0.edlqo.mongodb.net/Mernstack ?retryWrites=true&w=majority&appName=Cluster0'
+
 
 const connectDB = async () => {
-    return mongoose.connect(local_url)
+    return mongoose.connect(live_URL)
     .then(()=>{
         console.log('Connected to the database');
     }).catch((error)=>{
